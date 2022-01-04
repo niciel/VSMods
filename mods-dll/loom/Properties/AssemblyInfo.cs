63a091934640ed025427f023ffa940d8f373ad8a
niciel <damian.majcher.q@gmail.com>
2022-01-04 18:42:36 +0100

    Dodaj pliki projektów.

---
+++ mods-dll/loom/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Vintagestory.API.Common;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("loom")]
+[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b41a597-5dd5-4120-bf93-2deb4024ff55")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: ModInfo("loom", "loom",
+    Version = "1.0.0",
+    Description = "To be added",
+    Authors = new[] { "Unkown" })]
+
+[assembly: ModDependency("game")]
